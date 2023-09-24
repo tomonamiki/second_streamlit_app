46f682abbffd0cb305d17e1dcf1224e92bf2edd6
tomonamiki <145305907+tomonamiki@users.noreply.github.com>
2023-09-24 15:35:58 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 import pandas
 import requests
-import snowflake.connector
-from urllib.error import URLError
+＃　import snowflake.connector
+＃　from urllib.error import URLError
+
+streamlit hello
 
 streamlit.title('Professional Services & Training Biz Dashboard')
 streamlit.header('Quartery Trends')
